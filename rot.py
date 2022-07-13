acaf1484ebdf246507010778417de64192bd7218
/DarkAxe <107263975+DarkAx@users.noreply.github.com>
2022-07-13 08:03:38 +0300

    Update rot.py

--- rot.py
+++ rot.py
@@ -1,13 +1,24 @@
 #!/usr/bin/env python3
-def rot13(text):
-	a=[chr(i) for i in range(97,123)]
-	A=[chr(i) for i in range(65,91)]
-	result=""
-	for i in text:
-		if i.islower():
-			result+=a[(a.index(i)+13) % 26]
-		elif i.isupper():
-			result+=A[(A.index(i)+13) % 26]
-		else:
-			result+=i
-	return result
+def rot(text, num, encrypt=False): # If you want to encrypt just pass to the function a 'True' value, like this one: print(rot('sultan',13,True))
+    if encrypt:
+        a=[chr(i) for i in range(97,123)]; A=[chr(i) for i in range(65,91)]; result=""
+        for i in text:
+            if i.islower():
+                result+=a[(a.index(i)-num) % 26]
+            elif i.isupper():
+                result+=A[(A.index(i)-num) % 26]
+            else:
+                result+=i
+        return result
+    
+    else:
+
+        a=[chr(i) for i in range(97,123)]; A=[chr(i) for i in range(65,91)]; result=""
+        for i in text:
+            if i.islower():
+                result+=a[(a.index(i)+num) % 26]
+            elif i.isupper():
+                result+=A[(A.index(i)+num) % 26]
+            else:
+                result+=i
+        return result
