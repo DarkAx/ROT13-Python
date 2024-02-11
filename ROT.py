173271780e9baa5ba2f45dd5b8318bb056e1ad26
Sultan Bazher <107263975+SultanCYB@users.noreply.github.com>
2024-02-11 16:35:17 +0300

    Update ROT.py

--- ROT.py
+++ ROT.py
@@ -1,20 +1,14 @@
-def ROT(text, num, encrypt=False): # where num is 1 - 25
-	'''Any shift cipher from ROT 1 to ROT 25
-	   
-	
-		Usage: ROT(text, num)
-
-	   To encrypt just pass to the function a 'True' argument like this one : 
-
-				print(ROT('sultan', 13 ,True))'''
+def ROT(text, encrypt=False):
 
 		
-	a=[chr(i) for i in range(97,123)]; A=[chr(i) for i in range(65,91)]; result=""
+a=[chr(i) for i in range(97,123)]; A=[chr(i) for i in range(65,91)]; result=""
 
+if encrypt: num = 13
+else: num = -13
+	
 	for i in text:
 	    
-		if not encrypt:
-		    num = -num
+		
 			
 		if i.islower():
 				result+=a[(a.index(i)+num) % 26]
